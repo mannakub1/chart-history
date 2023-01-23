a0b14ff6dde29f66a19d6ceba067556a55f1f25f
Warut Tan <itanprscritical@gmail.com>
2023-01-23 17:57:17 +0700

    commit

--- src/constants/colors/colors.tsx
+++ src/constants/colors/colors.tsx
@@ -1,4 +1,5 @@
 export const BLUE_004C7C ='#004C7C'
 export const WHITE_FFFFFF = '#FFFFFF'
+export const BLUE_025BB7 = '#025BB7'
 
 export const GRADIENT_PRIMARY = 'linear-gradient(273.79deg, #025BB7 0%, #1894DF 100%)'
