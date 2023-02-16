0ff9bbbdc4da27a1f5d4fbeb544ad72a508af3d5
Warut Tan <itanprscritical@gmail.com>
2023-02-16 16:22:54 +0700

    Not found page
    Not found : Add not found page
    routes : Add route path not found

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,18 @@
 import { Route, Routes as RoutesLib, Navigate } from "react-router-dom";
 import Home from "../pages/Home";
+import NotFound from "../pages/NotFound";
 import * as paths from "../constants/paths";
 
 export const Routes = () => {
   return (
     <RoutesLib>
       <Route path={paths.home()} element={<Home />} errorElement={<Home />} />
-      <Route path="*" element={<Navigate to={paths.home()} replace />} />
+      <Route
+        path="/not-found"
+        element={<NotFound />}
+        errorElement={<NotFound />}
+      />
+      <Route path="*" element={<Navigate to="/not-found" replace />} />
     </RoutesLib>
   );
 };
